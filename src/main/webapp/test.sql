73fe7107fe52e22ff3766c4ba90f5be022b4fc62
iminto <waitfox@qq.com>
2016-01-16 00:39:16 +0800

    建标语句更新

--- src/main/webapp/test.sql
+++ src/main/webapp/test.sql
@@ -21,18 +21,18 @@ USE `test`;
 DROP TABLE IF EXISTS `p2p_account`;
 
 CREATE TABLE `p2p_account` (
-  `user_id` int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT '用户ID',
-  `total_money` bigint(15) DEFAULT '0' COMMENT '资金总额，单位分',
-  `use_money` bigint(15) DEFAULT '0' COMMENT '可用资金',
-  `frost_money` bigint(15) DEFAULT '0' COMMENT '冻结资金',
-  `have_interest` bigint(15) DEFAULT '0' COMMENT '已收利息',
-  `wait_interest` bigint(15) DEFAULT '0' COMMENT '待收利息',
-  `wait_total_money` bigint(15) DEFAULT '0' COMMENT '待收总额',
-  `exp_money` bigint(15) DEFAULT '0' COMMENT '体验金',
-  `exp_use_money` bigint(15) DEFAULT '0' COMMENT '已用体验金',
+  `useId` int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT '用户ID',
+  `totalMoney` bigint(15) DEFAULT '0' COMMENT '资金总额，单位分',
+  `useMoney` bigint(15) DEFAULT '0' COMMENT '可用资金',
+  `frostMoney` bigint(15) DEFAULT '0' COMMENT '冻结资金',
+  `haveInterest` bigint(15) DEFAULT '0' COMMENT '已收利息',
+  `waitInterest` bigint(15) DEFAULT '0' COMMENT '待收利息',
+  `waitTotalmoney` bigint(15) DEFAULT '0' COMMENT '待收总额',
+  `expMoney` bigint(15) DEFAULT '0' COMMENT '体验金',
+  `expUsemoney` bigint(15) DEFAULT '0' COMMENT '已用体验金',
   `sign` varchar(255) DEFAULT NULL COMMENT '验签，备用字段',
-  `is_warning` tinyint(1) DEFAULT NULL,
-  PRIMARY KEY (`user_id`)
+  `isWarning` tinyint(1) DEFAULT NULL,
+  PRIMARY KEY (`useId`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='用户资金表';
 
 /*Data for the table `p2p_account` */
@@ -43,7 +43,7 @@ DROP TABLE IF EXISTS `p2p_account_bank`;
 
 CREATE TABLE `p2p_account_bank` (
   `bid` int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT '银行卡ID',
-  `user_id` int(10) NOT NULL COMMENT '用户ID',
+  `userId` int(10) NOT NULL COMMENT '用户ID',
   `status` int(2) NOT NULL DEFAULT '1' COMMENT '银行卡状态(0,关闭;1,激活)',
   `cardno` varchar(24) NOT NULL COMMENT '银行卡号',
   `bank` int(10) NOT NULL COMMENT '银行ID',
@@ -63,17 +63,17 @@ DROP TABLE IF EXISTS `p2p_account_cash`;
 
 CREATE TABLE `p2p_account_cash` (
   `cid` int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT '提现ID',
-  `user_id` bigint(18) NOT NULL COMMENT '用户ID',
-  `c_status` tinyint(2) NOT NULL DEFAULT '0' COMMENT '状态:0未审，1通过，2失败',
+  `userId` bigint(18) NOT NULL COMMENT '用户ID',
+  `cashStatus` tinyint(2) NOT NULL DEFAULT '0' COMMENT '状态:0未审，1通过，2失败',
   `cardno` varchar(24) NOT NULL COMMENT '银行卡号',
   `bank` int(18) DEFAULT NULL COMMENT '银行',
   `branch` varchar(255) DEFAULT NULL COMMENT '银行支行',
   `money` bigint(15) NOT NULL COMMENT '提现金额',
   `realmoney` bigint(15) DEFAULT NULL COMMENT '到账金额',
   `fee` bigint(15) DEFAULT NULL COMMENT '提现手续费',
-  `verify_user` int(10) DEFAULT NULL COMMENT '提现审核人员',
-  `verify_time` int(10) DEFAULT NULL COMMENT '提现审核时间',
-  `verify_remark` varchar(255) DEFAULT NULL COMMENT '提现审核备注',
+  `verifyUser` int(10) DEFAULT NULL COMMENT '提现审核人员',
+  `verifyTime` int(10) DEFAULT NULL COMMENT '提现审核时间',
+  `verifyRemark` varchar(255) DEFAULT NULL COMMENT '提现审核备注',
   `addtime` int(10) NOT NULL COMMENT '添加时间',
   `addip` varchar(15) NOT NULL COMMENT '添加IP',
   PRIMARY KEY (`cid`)
@@ -86,22 +86,22 @@ CREATE TABLE `p2p_account_cash` (
 DROP TABLE IF EXISTS `p2p_account_log`;
 
 CREATE TABLE `p2p_account_log` (
-  `log_id` int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT '资金记录id',
-  `user_id` int(10) DEFAULT NULL COMMENT '用户id',
-  `log_money` bigint(15) DEFAULT NULL COMMENT '资金变动金额，单位分',
-  `log_type` tinyint(2) DEFAULT NULL COMMENT '变动类型（1，收入；2，支出）',
-  `account_type` varchar(64) DEFAULT NULL COMMENT '资金类型',
-  `total_money` bigint(15) DEFAULT NULL COMMENT '用户资金总额',
-  `use_money` bigint(15) DEFAULT NULL COMMENT '用户可用资金',
-  `frost_money` bigint(15) DEFAULT NULL COMMENT '用户冻结资金',
-  `have_interest` bigint(15) DEFAULT NULL,
-  `wait_interest` bigint(15) DEFAULT NULL,
-  `wait_total_money` bigint(15) DEFAULT NULL,
-  `exp_money` bigint(15) DEFAULT NULL COMMENT '体验金',
+  `logid` int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT '资金记录id',
+  `userId` int(10) DEFAULT NULL COMMENT '用户id',
+  `logMoney` bigint(15) DEFAULT NULL COMMENT '资金变动金额，单位分',
+  `logType` tinyint(2) DEFAULT NULL COMMENT '变动类型（1，收入；2，支出）',
+  `accountType` varchar(64) DEFAULT NULL COMMENT '资金类型',
+  `totalMoney` bigint(15) DEFAULT NULL COMMENT '用户资金总额',
+  `useMoney` bigint(15) DEFAULT NULL COMMENT '用户可用资金',
+  `frostMoney` bigint(15) DEFAULT NULL COMMENT '用户冻结资金',
+  `haveInterest` bigint(15) DEFAULT NULL,
+  `waitInterest` bigint(15) DEFAULT NULL,
+  `waitTotalmoney` bigint(15) DEFAULT NULL,
+  `expMoney` bigint(15) DEFAULT NULL COMMENT '体验金',
   `remark` varchar(255) DEFAULT NULL COMMENT '备注',
   `addtime` int(10) DEFAULT NULL COMMENT '变动时间',
   `addip` varchar(15) DEFAULT NULL,
-  PRIMARY KEY (`log_id`)
+  PRIMARY KEY (`logid`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='资金日志表';
 
 /*Data for the table `p2p_account_log` */
@@ -113,17 +113,17 @@ DROP TABLE IF EXISTS `p2p_account_recharge`;
 CREATE TABLE `p2p_account_recharge` (
   `rid` int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT '充值ID',
   `billno` varchar(128) NOT NULL COMMENT '充值订单流水',
-  `user_id` int(10) NOT NULL COMMENT '充值用户id',
+  `userId` int(10) NOT NULL COMMENT '充值用户id',
   `status` tinyint(2) NOT NULL DEFAULT '0' COMMENT '充值状态，0申请，1成功，2失败，',
   `money` bigint(15) NOT NULL COMMENT '充值金额',
-  `realmoney` bigint(15) NOT NULL COMMENT '实际到账金额',
-  `fee` bigint(15) NOT NULL COMMENT '充值手续费',
+  `realMoney` bigint(15) NOT NULL COMMENT '实际到账金额',
+  `fee` int(15) NOT NULL COMMENT '充值手续费',
   `type` int(11) NOT NULL COMMENT '充值类型1,线上，2，线下',
-  `recharge_type` varchar(128) DEFAULT NULL COMMENT '充值类型，对应充值通道id',
+  `rechargeType` varchar(128) DEFAULT NULL COMMENT '充值类型，对应充值通道id',
   `ret` varchar(1024) DEFAULT NULL COMMENT '线上充值返回参数序列化存储',
-  `verify_user` int(10) DEFAULT NULL COMMENT '审核人',
-  `verify_time` int(10) DEFAULT NULL COMMENT '审核时间',
-  `verify_remark` varchar(255) DEFAULT NULL COMMENT '审核备注',
+  `verifyUser` int(10) DEFAULT NULL COMMENT '审核人',
+  `verifyTime` int(10) DEFAULT NULL COMMENT '审核时间',
+  `verifyRemark` varchar(255) DEFAULT NULL COMMENT '审核备注',
   `addtime` int(10) NOT NULL COMMENT '添加时间',
   `addip` varchar(15) NOT NULL COMMENT '添加IP',
   PRIMARY KEY (`rid`)
@@ -136,21 +136,21 @@ CREATE TABLE `p2p_account_recharge` (
 DROP TABLE IF EXISTS `p2p_article`;
 
 CREATE TABLE `p2p_article` (
-  `article_id` int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT '文章ID',
-  `article_cat_id` int(10) DEFAULT NULL COMMENT '文章分类ID',
-  `user_id` int(10) DEFAULT NULL COMMENT '发布用户ID',
-  `show_status` tinyint(2) NOT NULL DEFAULT '1' COMMENT '显示状态(0,不显示；1，显示)',
-  `article_title` varchar(96) DEFAULT NULL COMMENT '文章标题',
-  `article_pic` varchar(96) DEFAULT NULL COMMENT '文章图片',
-  `article_desc` varchar(255) DEFAULT NULL COMMENT '文章简介',
-  `article_content` varchar(20000) DEFAULT NULL COMMENT '文章内容',
-  `acc_num` int(10) DEFAULT NULL COMMENT '访问次数',
+  `articleId` int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT '文章ID',
+  `articleCatId` int(10) DEFAULT NULL COMMENT '文章分类ID',
+  `userId` int(10) DEFAULT NULL COMMENT '发布用户ID',
+  `showStatus` tinyint(2) NOT NULL DEFAULT '1' COMMENT '显示状态(0,不显示；1，显示)',
+  `articleTitle` varchar(96) DEFAULT NULL COMMENT '文章标题',
+  `articlePic` varchar(96) DEFAULT NULL COMMENT '文章图片',
+  `articleDesc` varchar(255) DEFAULT NULL COMMENT '文章简介',
+  `articleContent` varchar(20000) DEFAULT NULL COMMENT '文章内容',
+  `accNum` int(10) DEFAULT NULL COMMENT '访问次数',
   `sort` int(5) DEFAULT '0' COMMENT '排序权重',
   `addtime` int(10) DEFAULT NULL COMMENT '添加时间',
   `addip` varchar(15) DEFAULT NULL COMMENT '文章添加ip',
   `updatetime` int(10) DEFAULT NULL COMMENT '修改时间',
   `issupport` tinyint(2) DEFAULT '0' COMMENT '是否推荐（0，默认；1，推荐；）',
-  PRIMARY KEY (`article_id`)
+  PRIMARY KEY (`articleId`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='文章表';
 
 /*Data for the table `p2p_article` */
@@ -160,62 +160,106 @@ CREATE TABLE `p2p_article` (
 DROP TABLE IF EXISTS `p2p_articlecat`;
 
 CREATE TABLE `p2p_articlecat` (
-  `article_cat_id` int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT '文章分类ID',
-  `article_cat_name` varchar(64) DEFAULT NULL COMMENT '文章分类名称',
-  `article_cat_alias` varchar(32) DEFAULT NULL COMMENT '类别别名',
-  `article_cat_desc` varchar(255) DEFAULT NULL COMMENT '文章分类简介',
-  `cat_type` tinyint(1) DEFAULT NULL COMMENT '栏目类别（1，列表页；2，单页3，封面页）',
-  `show_status` tinyint(1) DEFAULT NULL COMMENT '是否显示（0，不显示；1，显示）',
-  `p_id` int(10) DEFAULT NULL COMMENT '父栏目id',
+  `articleCatId` int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT '文章分类ID',
+  `articleCatName` varchar(64) DEFAULT NULL COMMENT '文章分类名称',
+  `articleCatAlias` varchar(32) DEFAULT NULL COMMENT '类别别名',
+  `articleCatDesc` varchar(255) DEFAULT NULL COMMENT '文章分类简介',
+  `catType` tinyint(1) DEFAULT NULL COMMENT '栏目类别（1，列表页；2，单页3，封面页）',
+  `showStatus` tinyint(1) DEFAULT NULL COMMENT '是否显示（0，不显示；1，显示）',
+  `pid` int(10) DEFAULT NULL COMMENT '父栏目id',
   `tree` varchar(256) DEFAULT NULL COMMENT '子分类等级',
-  `list_tmp_path` varchar(128) DEFAULT NULL COMMENT '列表模板路径',
-  `page_tmp_path` varchar(128) DEFAULT NULL COMMENT '内容模板路径',
-  `special_tmp_path` varchar(128) DEFAULT NULL COMMENT '专题模板路径',
+  `listTmpPath` varchar(128) DEFAULT NULL COMMENT '列表模板路径',
+  `pageTmpPath` varchar(128) DEFAULT NULL COMMENT '内容模板路径',
+  `specialTmpPath` varchar(128) DEFAULT NULL COMMENT '专题模板路径',
   `sort` int(8) DEFAULT NULL COMMENT '排序',
-  `pub_user_id` int(10) DEFAULT NULL COMMENT '发布用户ID',
+  `pubUserId` int(10) DEFAULT NULL COMMENT '发布用户ID',
   `addtime` int(10) DEFAULT NULL COMMENT '添加时间',
   `updatetime` int(10) DEFAULT NULL COMMENT '修改时间',
-  PRIMARY KEY (`article_cat_id`)
+  PRIMARY KEY (`articleCatId`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='文章目录表';
 
 /*Data for the table `p2p_articlecat` */
+
+/*Table structure for table `p2p_project` */
+
+DROP TABLE IF EXISTS `p2p_project`;
+
+CREATE TABLE `p2p_project` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `proId` bigint(20) NOT NULL COMMENT '逻辑主键',
+  `proName` varchar(256) NOT NULL COMMENT '借款标题',
+  `proAccount` int(11) NOT NULL COMMENT '借款金额，单位分',
+  `proAccountYes` int(11) DEFAULT NULL COMMENT '已投标金额，单位分',
+  `proTimeLimit` int(11) DEFAULT NULL COMMENT '借款期限',
+  `proTimeUint` tinyint(4) DEFAULT NULL COMMENT '借款期限类型，1为天，2为月',
+  `proValidTime` int(11) DEFAULT NULL COMMENT '借款有效时间',
+  `proApr` double DEFAULT NULL COMMENT '借款利率',
+  `proStyle` tinyint(4) DEFAULT NULL COMMENT '还款方式',
+  `proStatus` tinyint(4) DEFAULT NULL COMMENT '标的状态 0初始1初审成功，投标中2初审失败3复审成功4复审失败5流标6撤销7正常结束',
+  `proVerifyUser` int(11) DEFAULT NULL COMMENT '审标人',
+  `proVerifyTime` int(11) DEFAULT NULL COMMENT '初审时间',
+  `proVerifyRemark` varchar(256) DEFAULT NULL COMMENT '初审备注',
+  `proFullVerifyUser` int(11) DEFAULT NULL COMMENT '满标审核人',
+  `proFullVerifyTime` int(11) DEFAULT NULL COMMENT '满标审核时间',
+  `proFullVerifyRemark` varchar(256) DEFAULT NULL COMMENT '满标审核备注',
+  `proType` tinyint(4) DEFAULT NULL COMMENT '1信用标2担保标3抵押标4秒标5定向标',
+  `proDxb` varchar(32) DEFAULT NULL COMMENT '定向标密码',
+  `proDesc` varchar(20000) DEFAULT NULL COMMENT '标的详细描述',
+  `proAwardType` tinyint(4) DEFAULT NULL COMMENT '奖励类型 1百分比2固定金额',
+  `proAward` double DEFAULT NULL COMMENT '奖励的具体数值',
+  `proLowAcount` int(11) DEFAULT NULL COMMENT '最低要求投资金额',
+  `proMostAccount` int(11) DEFAULT NULL COMMENT '标的最高允许投标金额',
+  `successTime` int(11) DEFAULT NULL COMMENT '满标时间',
+  `endTime` int(11) DEFAULT NULL COMMENT '根据有效期算出的截止时间',
+  `orderNum` mediumint(9) DEFAULT NULL COMMENT '投标次数',
+  `autoRatio` int(11) DEFAULT NULL COMMENT '允许的自动投标比例，百分比',
+  `repayment` int(11) DEFAULT NULL COMMENT '还款金额',
+  `repaymentYes` int(11) DEFAULT NULL COMMENT '已还金额',
+  `expansion` varchar(512) DEFAULT NULL COMMENT '扩充字段',
+  `addtime` int(11) DEFAULT NULL,
+  `addip` varchar(15) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `bid` (`proId`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='借款标基本信息';
+
+/*Data for the table `p2p_project` */
 
 /*Table structure for table `p2p_user` */
 
 DROP TABLE IF EXISTS `p2p_user`;
 
 CREATE TABLE `p2p_user` (
-  `user_id` int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT '用户id',
-  `user_name` varchar(16) NOT NULL COMMENT '用户登录名',
-  `login_pass` varchar(32) NOT NULL COMMENT '登陆密码',
-  `pay_pass` varchar(32) DEFAULT NULL COMMENT '支付密码',
+  `userId` int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT '用户id',
+  `userName` varchar(16) NOT NULL COMMENT '用户登录名',
+  `loginPass` varchar(32) NOT NULL COMMENT '登陆密码',
+  `payPass` varchar(32) DEFAULT NULL COMMENT '支付密码',
   `email` varchar(64) NOT NULL COMMENT '用户邮箱',
   `phone` varchar(11) DEFAULT NULL COMMENT '用户手机',
-  `home_tel` varchar(16) DEFAULT NULL,
-  `user_qq` bigint(20) DEFAULT NULL COMMENT '用户QQ',
-  `user_pic` varchar(64) DEFAULT NULL COMMENT '用户头像',
-  `real_name` varchar(16) DEFAULT NULL COMMENT '真实姓名',
-  `card_num` varchar(30) DEFAULT '' COMMENT '证件号码',
-  `user_sex` tinyint(1) unsigned DEFAULT NULL COMMENT '用户性别（0，未知；1，男；2，女）',
-  `user_age` smallint(5) unsigned DEFAULT NULL COMMENT '用户年龄',
-  `user_edu` tinyint(1) DEFAULT NULL COMMENT '用户学历（0，未知；1，小学；2，初中；3，高中/高专；4，大专；5，本科；6，硕士/博士/及以上）',
-  `birth_place` varchar(128) DEFAULT NULL COMMENT '出生地',
-  `live_place` varchar(128) DEFAULT NULL COMMENT '居住地',
-  `user_address` varchar(128) DEFAULT NULL COMMENT '用户联系地址',
-  `invite_user_id` int(11) DEFAULT NULL COMMENT '父id/推荐人id',
-  `user_type` tinyint(2) DEFAULT '0' COMMENT '用户类型（0，普通注册用户；1，手机注册用户2，后台手动添加用户）',
-  `is_email_check` tinyint(1) DEFAULT '0' COMMENT '邮箱认证（0，未认证；1已认证）',
-  `is_phone_check` tinyint(1) DEFAULT '0' COMMENT '手机认证（0，未认证；1已认证）',
-  `is_realname_check` tinyint(1) DEFAULT '0' COMMENT '实名认证（0，未认证；1已认证）',
-  `is_safequestion_check` tinyint(1) DEFAULT '0' COMMENT '密保问题（0，未设置；1，已设置）',
-  `vip_stop_time` int(10) DEFAULT NULL COMMENT 'VIP到期时间',
-  `is_lock` tinyint(1) DEFAULT '0' COMMENT '账户锁定（0，正常；1，锁定）',
-  `register_time` int(10) DEFAULT NULL COMMENT '注册时间',
-  `login_time` int(10) DEFAULT NULL COMMENT '登录时间',
-  `register_ip` varchar(15) DEFAULT NULL,
-  `invite_num` int(11) DEFAULT '0' COMMENT '邀请人数',
-  PRIMARY KEY (`user_id`),
-  KEY `i_username` (`user_name`)
+  `homeTel` varchar(16) DEFAULT NULL,
+  `userQQ` bigint(20) DEFAULT NULL COMMENT '用户QQ',
+  `userPic` varchar(64) DEFAULT NULL COMMENT '用户头像',
+  `realname` varchar(16) DEFAULT NULL COMMENT '真实姓名',
+  `cardnum` varchar(30) DEFAULT '' COMMENT '证件号码',
+  `gender` tinyint(1) unsigned DEFAULT NULL COMMENT '用户性别（0，未知；1，男；2，女）',
+  `age` smallint(5) unsigned DEFAULT NULL COMMENT '用户年龄',
+  `edu` tinyint(1) DEFAULT NULL COMMENT '用户学历（0，未知；1，小学；2，初中；3，高中/高专；4，大专；5，本科；6，硕士/博士/及以上）',
+  `birthPlace` varchar(128) DEFAULT NULL COMMENT '出生地',
+  `livePlace` varchar(128) DEFAULT NULL COMMENT '居住地',
+  `userAddress` varchar(128) DEFAULT NULL COMMENT '用户联系地址',
+  `inviteUserId` int(11) DEFAULT NULL COMMENT '父id/推荐人id',
+  `userType` tinyint(2) DEFAULT '0' COMMENT '用户类型（0，普通注册用户；1，手机注册用户2，后台手动添加用户）',
+  `isEmailCheck` tinyint(1) DEFAULT '0' COMMENT '邮箱认证（0，未认证；1已认证）',
+  `isPhoneCheck` tinyint(1) DEFAULT '0' COMMENT '手机认证（0，未认证；1已认证）',
+  `isRealnameCheck` tinyint(1) DEFAULT '0' COMMENT '实名认证（0，未认证；1已认证）',
+  `isSafequestionCheck` tinyint(1) DEFAULT '0' COMMENT '密保问题（0，未设置；1，已设置）',
+  `vipStopTime` int(10) DEFAULT NULL COMMENT 'VIP到期时间',
+  `isLock` tinyint(1) DEFAULT '0' COMMENT '账户锁定（0，正常；1，锁定）',
+  `registerTime` int(10) DEFAULT NULL COMMENT '注册时间',
+  `loginTime` int(10) DEFAULT NULL COMMENT '登录时间',
+  `registerip` varchar(15) DEFAULT NULL,
+  `invitenum` int(11) DEFAULT '0' COMMENT '邀请人数',
+  PRIMARY KEY (`userId`),
+  KEY `i_username` (`userName`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 /*Data for the table `p2p_user` */
