cb182d85c1ae9edf558e976c76b35ad7d2fec342
iminto <waitfox@qq.com>
2016-03-23 22:03:11 +0800

    我要投资页完成部分。分页的处理暂只完成部分

--- src/main/java/com/baicai/controller/common/InvestController.java
+++ src/main/java/com/baicai/controller/common/InvestController.java
@@ -2,15 +2,24 @@ package com.baicai.controller.common;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import com.baicai.domain.system.Pagination;
+import com.baicai.service.project.ProjectService;
+import com.baicai.util.NumberHelper;
 
 @Controller
 @RequestMapping
 public class InvestController {
+	@Autowired
+	private ProjectService projectService;
 	
 	@RequestMapping("/invest")
 	public String invest(HttpServletRequest request,HttpServletResponse response){
+		Integer page=NumberHelper.str2int(request.getParameter("page"));
+		Pagination proJectList=projectService.getProjectList(page);
+		request.setAttribute("proList", proJectList);
 		return "/views/index/invest";		
 	}
 
--- src/main/java/com/baicai/service/project/ProjectService.java
+++ src/main/java/com/baicai/service/project/ProjectService.java
@@ -4,8 +4,10 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import com.baicai.core.BaseService;
+import com.baicai.core.DaoUtil;
 import com.baicai.dao.project.ProjectDAO;
 import com.baicai.domain.project.Project;
+import com.baicai.domain.system.Pagination;
 @Service
 public class ProjectService extends BaseService{
 	@Autowired
@@ -13,5 +15,18 @@ public class ProjectService extends BaseService{
 	
 	public  List<Project> findProjectsOnIndex(Integer cnt){
 		return projectDAO.findProjectsOnIndex(cnt);
+	}
+	
+	public Pagination getProjectList(Integer page){
+		Pagination pager=new Pagination();
+		String sql_where = " 1  ";
+		String countSQL="SELECT count(*)  FROM {project} WHERE "+sql_where;
+		int count=dao.queryForInt(DaoUtil.format(countSQL));
+		pager.setTotal(count);
+		pager.setPage(page);
+		String sql="SELECT *  FROM {project} WHERE "+sql_where+" order by proStatus ASC LIMIT "+pager.getOffset()+", "+pager.getPageSize();
+		List<Project> list=dao.queryForList(Project.class, DaoUtil.format(sql));
+		pager.setList(list);		
+		return pager;
 	}
 }
