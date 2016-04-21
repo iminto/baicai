95406b1714fc188cda39ddfbf1651333e32a40af
iminto <waitfox@qq.com>
2016-04-21 22:08:54 +0800

    与注册登录相关的页面

--- src/main/java/com/baicai/controller/user/UserController.java
+++ src/main/java/com/baicai/controller/user/UserController.java
@@ -6,6 +6,8 @@ import javax.servlet.http.HttpServletResponse;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.baicai.domain.user.User;
+
 @Controller
 @RequestMapping("/user")
 public class UserController {
@@ -13,6 +15,16 @@ public class UserController {
 	@RequestMapping("/login")
 	public String login(HttpServletRequest request,HttpServletResponse response){
 		return "/views/user/login";
+	}
+	
+	@RequestMapping("/reg")
+	public String register(HttpServletRequest request,HttpServletResponse response){
+		return "/views/user/register";
+	}
+	
+	@RequestMapping("/doreg")
+	public String doRegister(HttpServletRequest request,HttpServletResponse response,User user){
+		return "/views/user/register";
 	}
 
 }
