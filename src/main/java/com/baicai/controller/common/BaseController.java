89eb3315d7dc5acda05c19c8b592b12cd48b388e
iminto <waitfox@qq.com>
2016-04-24 17:26:11 +0800

    添加一个控制器基类，暂不使用

---
+++ src/main/java/com/baicai/controller/common/BaseController.java
@@ -0,0 +1,12 @@
+package com.baicai.controller.common;
+/**
+ * 控制器基类，封装控制器的必须属性
+* @Description: TODO
+* @author 猪肉有毒 waitfox@qq.com  
+* @date 2016年4月24日 下午4:26:16 
+* @version V1.0  
+* 我只为你回眸一笑，即使不够倾国倾城，我只为你付出此生，换来生再次相守
+ */
+public class BaseController {
+
+}
