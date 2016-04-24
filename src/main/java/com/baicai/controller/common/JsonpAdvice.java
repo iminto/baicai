2bb129d645d89da5a8476c6fd13140e48e03d1e5
iminto <waitfox@qq.com>
2016-04-24 17:27:21 +0800

    jsonp优化

--- src/main/java/com/baicai/controller/common/JsonpAdvice.java
+++ src/main/java/com/baicai/controller/common/JsonpAdvice.java
@@ -1,4 +1,7 @@
 package com.baicai.controller.common;
+import org.springframework.http.MediaType;
+import org.springframework.http.server.ServerHttpRequest;
+import org.springframework.http.server.ServerHttpResponse;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.servlet.mvc.method.annotation.AbstractJsonpResponseBodyAdvice;
 /**
@@ -15,4 +18,10 @@ public class JsonpAdvice extends AbstractJsonpResponseBodyAdvice {
 	public JsonpAdvice() {        
 	    super("callback");    
 	  }
+	
+	@Override
+	protected MediaType getContentType( MediaType contentType, ServerHttpRequest request,
+			ServerHttpResponse response ) {
+		return new MediaType("application", "javascript", contentType.getCharSet());
+	}
 }
