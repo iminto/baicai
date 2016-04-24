0be4e2be82b0ee2977808e0782a6a9186b01238d
iminto <waitfox@qq.com>
2016-04-24 17:50:25 +0800

    完善基类

--- src/main/java/com/baicai/controller/common/BaseController.java
+++ src/main/java/com/baicai/controller/common/BaseController.java
@@ -1,4 +1,14 @@
 package com.baicai.controller.common;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.serializer.SerializerFeature;
+
 /**
  * 控制器基类，封装控制器的必须属性
 * @Description: TODO
@@ -7,6 +17,84 @@ package com.baicai.controller.common;
 * @version V1.0  
 * 我只为你回眸一笑，即使不够倾国倾城，我只为你付出此生，换来生再次相守
  */
-public class BaseController {
+public class BaseController implements HandlerInterceptor{
+	protected Logger logger = LoggerFactory.getLogger(this.getClass());
+	
+	/**
+	 * preHandle方法是进行处理器拦截用的，顾名思义，该方法将在Controller处理之前进行调用
+	 * @param request
+	 * @param response
+	 * @param handler
+	 * @return
+	 * @throws Exception
+	 */
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+			throws Exception {
+		return true;
+	}
+	
+	/**
+	 * 这个方法只会在当前这个Interceptor的preHandle方法返回值为true的时候才会执行。
+	 * postHandle是进行处理器拦截用的，它的执行时间是在处理器进行处理之后，也就是在Controller的方法调用之后执行，但是它会在DispatcherServlet进行视图的渲染之前执行.
+	 * 也就是说在这个方法中你可以对ModelAndView进行操做
+	 * @param request
+	 * @param response
+	 * @param handler
+	 * @param modelAndView
+	 * @throws Exception
+	 */
+	@Override
+	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
+			ModelAndView modelAndView) throws Exception {
+		
+	}
+	
+	/**
+	 * 该方法也是需要当前对应的Interceptor的preHandle方法的返回值为true时才会执行。该方法将在整个请求完成之后，
+	 * 也就是DispatcherServlet渲染了视图执行，这个方法的主要作用是用于清理资源的
+	 * @param request
+	 * @param response
+	 * @param handler
+	 * @param ex
+	 * @throws Exception
+	 */
+	@Override
+	public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex)
+			throws Exception {
+	}
+	
+	/**
+	 * 重定向至地址 url
+	 * @param url 请求地址
+	 * @return
+	 */
+	protected String redirectTo(String url) {
+		StringBuilder rto = new StringBuilder("redirect:");
+		rto.append(url);
+		return rto.toString();
+	}
+	
+	/**
+	 * 返回 JSON 格式对象
+	 * @param object 转换对象
+	 * @return
+	 */
+	protected String toJson(Object object) {
+		return JSON.toJSONString(object, SerializerFeature.BrowserCompatible);
+	}
+
+	/**
+	 * 返回 JSON 格式对象
+	 * @param object 转换对象
+	 * @param format 序列化格式
+	 * @return
+	 */
+	protected String toJson(Object object, String format) {
+		if (format == null) {
+			return toJson(object);
+		}
+		return JSON.toJSONStringWithDateFormat(object, format, SerializerFeature.WriteDateUseDateFormat);
+	}
 
 }
