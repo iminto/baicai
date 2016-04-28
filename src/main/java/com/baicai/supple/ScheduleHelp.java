fcf1afea173d71964cf8deadd744bb9cd9b29440
iminto <waitfox@qq.com>
2016-04-29 00:25:17 +0800

    实现了对spring task的管理

--- src/main/java/com/baicai/supple/ScheduleHelp.java
+++ src/main/java/com/baicai/supple/ScheduleHelp.java
@@ -1,5 +1,14 @@
 package com.baicai.supple;
 
+import java.util.ArrayList;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.scheduling.config.CronTask;
+import org.springframework.scheduling.config.ScheduledTaskRegistrar;
+import org.springframework.scheduling.config.Task;
+import org.springframework.scheduling.support.ScheduledMethodRunnable;
 import org.springframework.stereotype.Component;
 
 /**
@@ -12,6 +21,7 @@ import org.springframework.stereotype.Component;
 * 我只为你回眸一笑，即使不够倾国倾城，我只为你付出此生，换来生再次相守
  */
 @Component
+@EnableScheduling
 public class ScheduleHelp {
 	
 	/**
@@ -22,11 +32,44 @@ public class ScheduleHelp {
 		System.out.println("ScheduleHelp...task1");
 	}
 	
+	@Scheduled(cron="0 0 2 * * *")
+	public void task3(){
+		System.out.println("ScheduleHelp...task1");
+	}
+	
 	/**
 	 * 心跳更新。启动时执行一次，之后每隔1000秒执行一次
 	 */
 	public void task2(){
 		System.out.println("ScheduleHelp...task2【心跳更新。启动时执行一次，之后每隔1000秒执行一次】");
+	}
+	
+	@Autowired   
+	private ScheduledTaskRegistrar scheduledTaskRegistrar;
+	
+	public List<Task> getScheduledTasks() {
+	    List<Task> result = new ArrayList<Task>();
+	    result.addAll(this.scheduledTaskRegistrar.getTriggerTaskList());
+	    result.addAll(this.scheduledTaskRegistrar.getCronTaskList());
+	    result.addAll(this.scheduledTaskRegistrar.getFixedRateTaskList());
+	    result.addAll(this.scheduledTaskRegistrar.getFixedDelayTaskList());
+	    return result;
+	}
+	
+	/**
+	 * 需要注意的是，只有在XML中定义的task才能被观察到，通过注解配置的是观察不到的
+	 * 至少在spring 4.2版本中还没有实现观察注解中的定时任务。
+	 * https://jira.spring.io/browse/SPR-12748
+	 * @return
+	 */
+	public List<CronTask> getScheduledCronTasks() {
+	    List<CronTask> cronTaskList = this.scheduledTaskRegistrar.getCronTaskList();
+	    for (CronTask cronTask : cronTaskList) {
+	        System.out.println("cron表达式："+cronTask.getExpression());
+	        ScheduledMethodRunnable scheduledMethodRunnable = (ScheduledMethodRunnable) cronTask.getRunnable();
+	        System.out.println("执行的方法："+scheduledMethodRunnable.getTarget().getClass().getName()+"@"+scheduledMethodRunnable.getMethod().getName());
+	    }
+	    return cronTaskList;
 	}
 
 }
