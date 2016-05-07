e2f1222cb5280138be6c31d8cffbefaf33c58c48
iminto <waitfox@qq.com>
2016-05-07 18:35:08 +0800

    常量统一管理

---
+++ src/main/java/com/baicai/core/Constant.java
@@ -0,0 +1,14 @@
+package com.baicai.core;
+
+public interface Constant {
+	public static final String CHARSET = "UTF-8";
+    public static final int CONCURRENT_CAPACITY_SIZE = 500;
+    public static final int LOCKER_SLEEP_UNIT_TIME = 20;
+
+    public static final int BUFFER_BYTE_SIZE = 8192;
+    public static final String DOT=".";
+    public static final String NULL = "null";
+    public static final String SPACE = " ";
+    public static final String QUOTE = "\"";
+    public static int ioBufferSize = 16384;
+}
