6003f615bf0672969d61ba30217d8120799df334
iminto <waitfox@qq.com>
2016-06-02 23:06:39 +0800

    规范路径

--- src/main/java/com/baicai/controller/index/IndexController.java
+++ src/main/java/com/baicai/controller/index/IndexController.java
@@ -1,4 +1,4 @@
-package com.baicai.controller.common;
+package com.baicai.controller.index;
 
 import java.util.List;
 import javax.servlet.http.HttpServletRequest;
@@ -6,6 +6,8 @@ import javax.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.baicai.controller.common.BaseController;
 import com.baicai.domain.project.Project;
 import com.baicai.service.project.ProjectService;
 
--- src/main/java/com/baicai/controller/index/InvestController.java
+++ src/main/java/com/baicai/controller/index/InvestController.java
@@ -1,10 +1,12 @@
-package com.baicai.controller.common;
+package com.baicai.controller.index;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.baicai.controller.common.BaseController;
 import com.baicai.domain.system.Pagination;
 import com.baicai.service.project.ProjectService;
 import com.baicai.util.NumberHelper;
