b011781545bd2f313435692091073631d4e53bd1
iminto <waitfox@qq.com>
2016-06-06 00:07:08 +0800

    安全中心的页面

---
+++ src/main/java/com/baicai/controller/user/SafeCenterController.java
@@ -0,0 +1,24 @@
+package com.baicai.controller.user;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import com.baicai.controller.common.BaseController;
+/**
+ * 
+* @Description: 安全中心
+* @author 猪肉有毒 waitfox@qq.com  
+* @date 2016年6月5日 下午11:50:40 
+* @version V1.0  
+* 我只为你回眸一笑，即使不够倾国倾城，我只为你付出此生，换来生再次相守
+ */
+@Controller
+@RequestMapping("/safecenter")
+public class SafeCenterController  extends BaseController{
+	
+	@RequestMapping("/index")
+	public String login(HttpServletRequest request, HttpServletResponse response) {
+		return "/views/safecenter/index";
+	}
+}
