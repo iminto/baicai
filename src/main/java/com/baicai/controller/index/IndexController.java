8a75ead460a9c1e4bbf6663374e03b70ce40b7f9
iminto <waitfox@qq.com>
2016-06-05 21:03:16 +0800

    安全中心页面

--- src/main/java/com/baicai/controller/index/IndexController.java
+++ src/main/java/com/baicai/controller/index/IndexController.java
@@ -5,6 +5,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.baicai.controller.common.BaseController;
@@ -27,6 +28,11 @@ public class IndexController extends BaseController{
 	@RequestMapping("/manage")
 	public String main(HttpServletRequest request,HttpServletResponse response){
 		return "/manage/login";		
+	}
+	
+	@RequestMapping("/static/{page}")
+	public String staticPage(HttpServletRequest request, @PathVariable String page){
+		return "/views/static/"+page;
 	}
 
 }
